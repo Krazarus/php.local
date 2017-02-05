91b624044f9cf8676c7a3bbf2086b52cca14887a
Krazarus <shcherbakov1andrii@gmail.com>
2017-02-05 13:45:13 +0200

    fhfghfg

--- controllers/PagesController.php
+++ controllers/PagesController.php
@@ -1,1 +1,12 @@
 <?php
+
+Class PageController
+{
+    public function home()
+    {
+        die('home');
+        $users = App::get('database')->selectAll('users');
+
+        require 'views/index/index.view.php';
+    }
+}
